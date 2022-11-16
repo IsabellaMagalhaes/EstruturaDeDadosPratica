ce28926c5a960342d14296ebe7275b3a2b27252c
IsabellaMagalhaes <49318187+IsabellaMagalhaes@users.noreply.github.com>
2022-11-16 18:17:08 -0300

    Add files via upload

---
+++ src/lista2/main.java
@@ -0,0 +1,13 @@
+public class main {
+
+	public static void main(String[] args) {
+		Pilha p = new Pilha(2);
+		p.inserir(6.9);
+		p.inserir(5.9);
+		p.inserir(4.9);
+		p.inserir(4.9);
+		p.novoVetor();
+
+	}
+
+}
